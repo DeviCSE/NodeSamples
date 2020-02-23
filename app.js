6c1aed640abae4a8d41068eca02e73cbd37da748
Devi Das <devidas.cse07@gmail.com>
2020-02-23 12:46:43 +0530

    Added ...

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('This is a simple Node App'));
-app.get('/authorInfo', (req, res) => res.send('This is Devi Das'));
+app.get('/', (req, res) => res.send('This is a simple Node App...'));
+app.get('/authorInfo', (req, res) => res.send('This is Devi Das...'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
